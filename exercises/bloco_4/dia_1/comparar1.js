8e5817beaf4203a9003d80e2668b7e68276e44cf
maym4y <jennim.almeida@gmail.com>
2020-10-26 23:07:32 -0300

    Adicionados desafios 2, 3, 4 e 5

---
+++ exercises/bloco_4/dia_1/comparar1.js
@@ -0,0 +1,7 @@
+let a = 5;
+let b = 3;
+if (a > b){
+    console.log(a)
+} else {
+    console.log(b)
+}
---
+++ exercises/bloco_4/dia_1/desafio5.js
@@ -0,0 +1,6 @@
+let a = 90;
+let b = 60;
+let c = 20;
+let result = a+b+c;
+
+console.log(result==180)
